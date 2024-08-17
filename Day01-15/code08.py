fcdf93b31fb4e9e93000afa73e34479bc6d82e5b
zhoujunhui2 <zhoujunhui2@xdf.cn>
2024-08-17 17:51:00 +0800

    code8.py

--- Day01-15/code08.py
+++ Day01-15/code08.py
@@ -1,27 +1,34 @@
-class stu:
+class person:
     def __init__(self, name, age):
-        print('构造函数调用。。')
-        print('my name is', name, 'age is ', age)
-        self.name = name
-        self.age = age
-        self.__name = name  # 私有变量通过在变量名前面加2个下划线
+        self._name = name
+        self._age = age
+        print('person构造函数')
+
+    def intro_self(self):
+        print('my name is ', self._name, self._age)
+
+
+class stu(person):  # 继承person
+    def __init__(self, name, age, grade):
+        super().__init__(name, age)
+        print('stu构造函数调用。。')
+        self._grade = grade
 
     def study(self, class_name):
-        print('%s正在学习%s' % (self.__name, class_name))
+        print('%s正在学习%s' % (self._name, class_name))
 
-    def watch_movie(self):
-        if self.age < 18:
-            print('%s只能看熊出没' % self.name)
-        else:
-            print('%s可以看岛国爱情动作大片' % self.name)
+    def intro_self(self):  # 重写父类方法
+        print('学生名是', self._name, self._age)
+
+    def intro_self2(self):
+        print('2学生名是', self._name, self._age)
 
 
 def main():
-    s1 = stu('zjh', 29)
-    print(s1.__name)  # 没有此字段
-    print(s1.age)
-    s1.study('python教程')
-    s1.watch_movie()
+    s = stu('zjh', 33, 100)
+    s.study('java')
+    s.intro_self()
+    s.intro_self2()
 
 
 if __name__ == '__main__':
